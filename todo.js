942e617d2361d57729b6615ca08853fd3565d6dc
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-01-26 22:48:04 +0100

    added saving to local storage

--- todo.js
+++ todo.js
@@ -1,28 +1,26 @@
-
 var form = document.querySelector("form");
 var ul = document.getElementById("list");
-
 form.addEventListener("submit", submit);
+//localStorage.clear();
+let listArr = [];
 
 function submit (event){
     event.preventDefault();
-
     var myInput = document.getElementById("myInput");
 
     if(myInput.value != ""){
-        newTask();
+        newTask(myInput.value);
+        listArr.push(myInput.value);
+        localStorage.setItem("list", JSON.stringify(listArr));
         myInput.value = "";
     }   
 }
 
-function newTask (){
+function newTask (myInput){
 
-    var ul = document.getElementById("list");
     var li = document.createElement("li");
     var inputEl = document.createElement("input");
-    var myInput = document.getElementById("myInput").value;
     var button = document.createElement("button");
-
     button.className = "delete";
     button.innerHTML = "x";
 
@@ -34,13 +32,18 @@ function newTask (){
     inputEl.setAttribute("type", "checkbox");
     button.addEventListener("click", deleteContent);
     inputEl.addEventListener("click", isTicked);
-
 }
 
 function deleteContent (e){
-   
+
     var targeted = e.target.parentNode;
-    targeted.remove(targeted);   
+    targeted.remove(targeted);
+    var toRemove = targeted.firstChild.nextSibling.data;
+    var iOfR = listArr.indexOf(toRemove)
+    if(iOfR > -1){
+        listArr.splice(iOfR,1)
+    }
+    localStorage.setItem("list", JSON.stringify(listArr));
 }
 
 function isTicked(e){
@@ -53,3 +56,15 @@ function isTicked(e){
     }
     isTicked = !isTicked;   
 }
+
+function checkStor(){ 
+if(localStorage.getItem("list")){
+    var data = JSON.parse(localStorage.getItem("list"));
+    listArr = JSON.parse(localStorage.getItem("list"))
+    data.forEach(item => {
+    newTask(item)})
+}else{
+    listArr = [];
+}
+}
+window.onload = checkStor();
